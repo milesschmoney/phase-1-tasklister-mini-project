4b4afd209530ab23bd1817b4f42136ac9ffceac1
Alex Ndaula <your_email@example.com>
2026-01-02 09:19:25 -0500

    Fix TaskLister submission logic

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const form = document.getElementById("create-task-form");
+  const input = document.getElementById("new-task-description");
+  const taskList = document.getElementById("tasks");
+
+  form.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    const taskText = input.value;
+
+    const li = document.createElement("li");
+    li.textContent = taskText;
+
+    taskList.appendChild(li);
+    input.value = "";
+  });
 });
